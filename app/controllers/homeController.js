522f5e84948cded741a69c9e709229ec31468406
Frode Bjerke <frode.bjerke@gmail.com>
2014-03-13 09:46:12 +0100

    render list of users recorded in a select

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,7 +1,12 @@
-
+var mongoose = require('mongoose'),
+  Wave = mongoose.model('Wave');
 
 exports.index = function (req, res) {
-  res.render('index', {
-    lol: "Mornsann!"
+  Wave.distinct('user', {}, function (err, resp) {
+    if (err) throw new Error(err);
+    res.render('index', {
+      users: resp
+    });
   });
+
 };
