6a65882b5dfe7884c20a8a0ef62f5f5e05c1b326
Frode Bjerke <frode.bjerke@gmail.com>
2014-03-26 20:24:54 +0100

    made path-items focusable

--- public/src/log-viz/views/addPathView.js
+++ public/src/log-viz/views/addPathView.js
@@ -24,6 +24,7 @@ define([
         }, this);
         controls.fetch();
       },
+      
       onShow: function () {
         this.delegateEvents();
       },
--- public/src/log-viz/views/pathsView.js
+++ public/src/log-viz/views/pathsView.js
@@ -5,16 +5,38 @@ define([
     var typeView = Marionette.ItemView.extend({
       template: Tmpl,
       tagName: 'li',
+      initialize: function (options) {
+        console.log(options.index);
+        this.index = options.index;
+      },
+      onRender: function () {
+        $(this.el).attr('tabindex', this.index);
+      },
       events: {
-        'click .paths-remove' : 'removePath'
+        'click .paths-remove' : 'removePath',
+        'focus' : 'hasFocus',
+        'blur' : 'lostFocus'
       },
       removePath: function (e) {
         this.model.destroy();
+      },
+      hasFocus: function () {
+        this.model.set("hasFocus", true);
+        this.render();
+      },
+      lostFocus: function () {
+        this.model.set("hasFocus", false);
+        this.render();
       }
     });
 
     return Marionette.CollectionView.extend({
       itemView: typeView,
       tagName: 'ul',
+      itemViewOptions: function (model, index) {
+        return {
+          index: index
+        };
+      }
     });
 });
